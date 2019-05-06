94bdae63c8ca33521ad5532fb213975d079528d3
firebussy <elementalanime555@gmail.com>
2019-05-06 14:28:17 -0700

    Add files via upload

---
+++ BookOfIllusions.java
@@ -0,0 +1,71 @@
+package book_of_illusions;
+
+/*
+ *    Copyright (c) The League of Amazing Programmers 2013-2019
+ *    Level 1
+ */
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.net.URL;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+ 
+/** We’re going to make a slideshow of cool optical illusions. When the user clicks on an illusion, a new one will be loaded. **/
+
+public class BookOfIllusions extends MouseAdapter {
+
+	// 1. Make a JFrame variable and initialize it using "new JFrame()"
+JFrame ill=new JFrame();
+JLabel sion;
+JLabel h;
+	public void run() {
+		// 2. make the frame visible
+		ill.setVisible(true);
+		// 3. set the size of the frame
+		ill.setSize(800, 800);
+		// 4. find 2 images and save them to your project’s default package
+		// 5. make a variable to hold the location of your image. e.g. "illusion.jpg"
+	String a= "yay.jpg";
+	String b="flow.jpg";
+
+		// 6. create a variable of type "JLabel" but don’t initialize it yet
+		 sion= loadImageFromComputer(a);h= loadImageFromComputer(b);
+		// 7. use the "loadImage..." methods below to initialize your JLabel
+		
+		// 8. add your JLabel to the frame
+		ill.add(sion);
+		// 9. call the pack() method on the frame
+		ill.pack();
+		// 10. add a mouse listener to your frame (hint: use *this*)
+		ill.addMouseListener(this);
+	}
+
+	public void mousePressed(MouseEvent e) {
+	
+		// 11. Print "clicked!" to the console when the mouse is pressed
+		System.out.println("Clicked!");
+		// 12. remove everything from the frame that was added earlier
+		// 13. load a new image like before (this is more than one line of code)\
+		
+		// 14. pack the frame
+		ill.pack();
+	}
+
+	// [OPTIONAL] 15. goad your users with some annoying or witty pop-ups
+
+	/*
+	 * To use this method, the image must be placed in your Eclipse project under "default package".
+	 */
+	public JLabel loadImageFromComputer(String fileName) {
+		URL imageURL = getClass().getResource(fileName);
+		Icon icon = new ImageIcon(imageURL);
+		return new JLabel(icon);
+	}
+
+}
+
+
---
+++ BookOfIllusionsRunner.java
@@ -0,0 +1,14 @@
+package book_of_illusions;
+
+/*
+ *    Copyright (c) The League of Amazing Programmers 2013-2019
+ *    Level 1
+ */
+
+public class BookOfIllusionsRunner {
+	public static void main(String[] args)  {
+		new BookOfIllusions().run();
+		
+	}
+	
+}
